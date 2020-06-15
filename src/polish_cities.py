a2c5708771cda67bc66781c283e397e80365032f
Maksim Korzh <freesoft.for.people@gmail.com>
2020-06-15 15:04:34 +0300

    added polish cities files

---
+++ src/polish_cities.py
@@ -0,0 +1,1 @@
+(fetch('https://en.wikipedia.org/wiki/List_of_postal_codes_in_Poland'), [open('cities.txt', 'a').write(city + '\n') for city in list(dict.fromkeys(list(filter(None, [text.strip() for text in response.css('li').css('a::text').getall()[:-55]]))).keys())])[-1]
