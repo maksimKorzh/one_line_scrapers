6bdf9ec62b722aaa78f7268563d78b2cd6615302
Maksim Korzh <freesoft.for.people@gmail.com>
2020-06-21 00:16:59 +0300

    added deobfuscated rightmove one liner

--- src/rightmove_deobfusctated.py
+++ src/rightmove_deobfusctated.py
@@ -19,7 +19,7 @@
 #    containing THIS file
 #
 # 2. Then copy and paste the following:
-#    exec(open('rightmove.py', 'r').read())
+#    exec(open('rightmove_deobfusctated.py', 'r').read())
 #
 #    You should've obtain CSV file
 #
@@ -33,7 +33,7 @@
 # 1. Invoke either SCRAPY or PYTHON Shell
 #
 # 2. Then copy and paste the following:
-#    print(' '.join([''.join(line.strip()) for line in open('rightmove.py', 'r').read().split('\n') if '#' not in line]))
+#    print(' '.join([''.join(line.strip()) for line in open('rightmove_deobfusctated.py', 'r').read().split('\n') if '#' not in line]))
 #  
 #    You should obtain this like line
 #    import csv; [ [ fetch('https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E87490&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false&index=' + str(page * 24)),  [ csv.DictWriter( open('rightmove.csv', 'a'), ['title', 'url', 'address', 'price', 'description', 'date']).writerow( { 'title': card.css('h2[class="propertyCard-title"]::text') .get() .strip(),  'url': card.css('a[class="propertyCard-link"]::attr(href)') .get(),  'address': card.css('address[class="propertyCard-address"] *::text') .getall()[-2],  'price': card.css('div[class="propertyCard-priceValue"]::text') .get() .strip(),  'description': card.css('span[itemprop="description"] *::text') .get(),  'date': ' '.join(list(filter(None, [ text.strip() for text in card.css('div[class="propertyCard-branchSummary"] *::text') .getall()])))  }  )  for card in response.css('div[class="propertyCard-wrapper"]')] ][-1]  for page in range(0, 4) ]
